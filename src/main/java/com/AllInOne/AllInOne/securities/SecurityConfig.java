08bf41ce6f08771ffb123092f3eb74871f9aafc7
Veluri Karthik <43465187+Karthik2508@users.noreply.github.com>
2025-04-15 00:31:40 +0530

    second commit

--- src/main/java/com/AllInOne/AllInOne/securities/SecurityConfig.java
+++ src/main/java/com/AllInOne/AllInOne/securities/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.AllInOne.AllInOne.securities;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +14,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.AllInOne.AllInOne.jwt.JwtConfig;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +30,9 @@ public class SecurityConfig {
 		this.serviceClass=serviceClass;
 	}
 	
+	@Autowired
+	private JwtConfig config;
+	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception
 	{
@@ -36,7 +44,10 @@ public class SecurityConfig {
 					registry.requestMatchers("/userManagement/**").permitAll();
 					registry.requestMatchers("/JwtManagement/**").permitAll();
 					registry.anyRequest().authenticated();
-				}).formLogin(formLogin -> formLogin.permitAll())
+				})
+				
+				.formLogin(formLogin -> formLogin.permitAll())
+				.addFilterBefore(config, UsernamePasswordAuthenticationFilter.class)
 				.build();	
 	}
 	                                                                                                                                                                                                                      
